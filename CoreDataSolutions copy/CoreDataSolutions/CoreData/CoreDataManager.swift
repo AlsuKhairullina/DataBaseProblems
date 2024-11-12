17997d44d9ee9db34757a5b2d70a53b57e8aafe0
Alsu Khairullina <alcukhairullina@yandex.ru>
2024-11-12 22:55:01 +0500

    coredata problem solved

--- CoreDataSolutions copy/CoreDataSolutions/CoreData/CoreDataManager.swift
+++ CoreDataSolutions copy/CoreDataSolutions/CoreData/CoreDataManager.swift
@@ -7,14 +7,64 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 class CoreDataManager {
 
     static let shared = CoreDataManager()
-    // TODO: add neccessary code
-
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "PhotosModel")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error {
+                fatalError("Unresolved error \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
     private init() {
         ValueTransformer.setValueTransformer(UIImageTransformer(), forName: NSValueTransformerName("UIImageTransformer"))
-        // TODO: add neccessary code
+    }
+    
+    func saveImage(_ image: UIImage, title: String) {
+        let fetchRequest: NSFetchRequest<Photo> = Photo.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "title == %@", title)
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            if let existingPhoto = results.first {
+                existingPhoto.content = image
+                print("Image updated successfully with title: \(title)")
+            } else {
+                let newPhoto = Photo(context: context)
+                newPhoto.content = image
+                newPhoto.title = title
+                print("New image saved successfully with title: \(title)")
+            }
+            try context.save()
+        } catch {
+            print("Failed to save or update image: \(error)")
+        }
+    }
+    
+    func fetchImage(withTitle title: String) -> UIImage? {
+        let fetchRequest: NSFetchRequest<Photo> = Photo.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "title == %@", title)
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            return results.first?.content
+        } catch {
+            print("Failed to fetch image: \(error)")
+            return nil
+        }
     }
 }
+
+
--- CoreDataSolutions copy/CoreDataSolutions/View/PhotoViewModel.swift
+++ CoreDataSolutions copy/CoreDataSolutions/View/PhotoViewModel.swift
@@ -10,24 +10,35 @@ import CoreData
 
 @MainActor
 final class PhotoViewModel: ObservableObject {
+    
+    private let dataManager: CoreDataManager
+    
     @Published var image: UIImage?
+    
     let urlString = "https://skitterphoto.com/photos/skitterphoto-12137-default.jpg"
 
-    // TODO: add neccessary code
-
-    init() {
-
+    init(dataManager: CoreDataManager = CoreDataManager.shared) {
+        self.dataManager = dataManager
+        fetchImageFromStorage()
     }
 
     func fetchImage() async {
         let url = URL(string: urlString)!
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-
-            // TODO: add neccessary code
-
-        } catch  {
-            print(error)
+            if let downloadedImage = UIImage(data: data) {
+                self.image = downloadedImage
+                dataManager.saveImage(downloadedImage, title: "default")
+            } else {
+                print("Failed to convert data to UIImage.")
+            }
+        } catch {
+            print("Failed to fetch image:", error)
         }
     }
+    
+    func fetchImageFromStorage() {
+        image = dataManager.fetchImage(withTitle: "default")
+    }
+
 }
